6cd20f0bc3ca68974f6fa21e0c21ea1aacf803da
Tithi Patel <155192074+tithipatel25@users.noreply.github.com>
2026-03-12 21:53:51 +0000

    03_01

--- Ex_Files/03_01_begin/main.py
+++ Ex_Files/03_01_begin/main.py
@@ -1,8 +1,9 @@
-import csv
-from pprint import pprint
+import csv #comma seperated values
+from pprint import pprint #print in the console in a readable way
 
 EINSTEIN_CSV = 'Albert,Einstein,1879-03-14,1955-04-18,Germany,"for his services to Theoretical Physics, and especially for his discovery of the law of the photoelectric effect",physics,1921'
 
+#dictionary - remember to put "" or ''
 EINSTEIN = {
     "birthplace": "Germany",
     "name": "Albert",
@@ -12,11 +13,11 @@ EINSTEIN = {
     "motivation": "for his services to Theoretical Physics...",
 }
 
-with open("laureates.csv", "r") as f:
+with open("laureates.csv", "r") as f: #opening the .csv file and reading it 'r', and assign it to f
     reader = csv.DictReader(f)
     laureates = list(reader)
 
 for laureate in laureates:
     if laureate["surname"] == "Einstein":
-        pprint(laureate)
+        pprint(laureate) #pretty print
         break
