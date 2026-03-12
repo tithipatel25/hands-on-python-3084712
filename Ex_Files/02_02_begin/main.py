9a2b71e7991430ee6c8741a494734db6b14abf23
Tithi Patel <155192074+tithipatel25@users.noreply.github.com>
2026-03-12 21:21:44 +0000

    02_02

--- Ex_Files/02_02_begin/main.py
+++ Ex_Files/02_02_begin/main.py
@@ -3,10 +3,15 @@ extened_grt = "Hello World, " + "this is a long string"
 
 name = "John"
 
-intrupution = f"Hello {name}"
+intrupution = f"Hello {name}" #string interpolation using opening and closing curly brackets
 
 greet_format = "Hello {}"
 
 formatted = greet_format.format(name)
 
 print(intrupution, formatted)
+
+print(formatted.upper()) #prints all upper case
+print(formatted.lower()) #prints all lower case
+print(formatted.replace("John", "Paul")) 
+  #prints in the same format as it initially did, except replaces "John" with "Paul"
