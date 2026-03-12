0ab9dd29a7e87b571487d06dbc6770b9ee7fe76f
Tithi Patel <155192074+tithipatel25@users.noreply.github.com>
2026-03-12 21:35:56 +0000

    02_04

--- Ex_Files/02_04_begin/main.py
+++ Ex_Files/02_04_begin/main.py
@@ -3,19 +3,21 @@ AGES = [20, 21, 22, 23]
 
 i = 0
 while i < len(NAMES):
-    print(NAMES[i], AGES[i])
-    i += 1
+    print(NAMES[i], AGES[i]) #print name and age on the index i
+    i += 1 #increment i
 
 for name in NAMES:
     print(name)
 
-for name, age in zip(NAMES, AGES):
+for name, age in zip(NAMES, AGES): #zip, zips the two together
     print(f"{name} {age}")
 
 for name in reversed(NAMES):
-    print(name)
+    print(name) #reverse names
 
 for i in range(5):
     print(i)
 
 # enumerate
+for i, name in enumerate(NAMES):
+    print(f"{i}{name}") #comes in handy when you want to know where you are at
