7b3bdb05eb663a32e0fbaf19388953a6356d8089
Tithi Patel <155192074+tithipatel25@users.noreply.github.com>
2026-03-12 21:42:16 +0000

    02_05

--- Ex_Files/02_05_begin/main.py
+++ Ex_Files/02_05_begin/main.py
@@ -6,7 +6,8 @@ STAGING = "staging"
 CODE_SPACE = "code_space"
 LOCAL = "local"
 
-current_env = os.environ.get("ENV_NAME", DEVELOPMENT)
+current_env = os.environ.get("ENV_NAME", DEVELOPMENT)#allows you to access the env that you're running on and grab things from there
+#to change the ENV_NAME, type export ENV_NAME='*insert env name*' into the terminal and then run"
 
 if current_env == DEVELOPMENT:
     print("Development environment")
@@ -14,7 +15,7 @@ elif current_env == PRODUCTION:
     print("Production environment")
 elif current_env == STAGING:
     print("Staging environment")
-elif current_env in [CODE_SPACE, LOCAL]:
+elif current_env in [CODE_SPACE, LOCAL]: #checking a collection of things
     print("Codespace or local environment")
 else:
     print("Unknown environment")
