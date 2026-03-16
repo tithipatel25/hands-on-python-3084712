da327238245c2b67af994f55852ddd38aea1b845
Tithi Patel <155192074+tithipatel25@users.noreply.github.com>
2026-03-16 20:32:53 +0000

    Some 03_03_begin changes

--- Ex_Files/03_03_begin/main.py
+++ Ex_Files/03_03_begin/main.py
@@ -2,7 +2,7 @@ import csv
 import json
 from pprint import pprint
 
-EINSTEIN = {
+EINSTEIN = { #python dictionary, looks a lot like json
     "birthplace": "Germany",
     "name": "Albert",
     "surname": "Einstein",
@@ -13,14 +13,14 @@ EINSTEIN = {
 
 einstein_json = json.dumps(EINSTEIN) #converts the above info (dictionary) into json
 back_to_dict = json.loads(einstein_json) #converst json to dictionary
-print(einstein_json)
-pprint(back_to_dict)
+print(einstein_json) #print
+pprint(back_to_dict) #pretty-print
 
 #the following codes turns the .csv file into a .json file
-with open("laureates.csv", "r") as f:
-    reader = csv.DictReader(f)
-    laureates = list(reader)
+with open("laureates.csv", "r") as f: #r is read mode
+    reader = csv.DictReader(f) #dictionary reader using the object f
+    laureates = list(reader) #creating a list of dictionaries, each one representing Noble prize winner
 
 
-with open("laureates.json", "w") as f:
-    json.dump(laureates, f, indent=2)
+with open("laureates.json", "w") as f: #w is write mode
+    json.dump(laureates, f, indent=2) #
