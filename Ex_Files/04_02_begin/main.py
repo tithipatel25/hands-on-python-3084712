98aaefea26c3af3d23a905dffd315595c56d4a63
Tithi Patel <155192074+tithipatel25@users.noreply.github.com>
2026-03-16 21:16:50 +0000

    04_02

--- Ex_Files/04_02_begin/main.py
+++ Ex_Files/04_02_begin/main.py
@@ -1,3 +1,10 @@
 from flask import Flask
 
-app = Flask(__name__)
+app = Flask(__name__) #instantiate a flask object
+
+@app.route("/") #define a route
+
+def hello():
+  return "Hello, World!"
+
+app.run(debug=True)
