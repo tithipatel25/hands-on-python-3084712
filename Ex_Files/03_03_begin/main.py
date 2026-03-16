d2b238f9d8a28532d473ebc724a2ccde1b758138
Tithi Patel <155192074+tithipatel25@users.noreply.github.com>
2026-03-16 20:09:18 +0000

    03_03

--- Ex_Files/03_03_begin/main.py
+++ Ex_Files/03_03_begin/main.py
@@ -11,11 +11,12 @@ EINSTEIN = {
     "motivation": "for his services to Theoretical Physics...",
 }
 
-einstein_json = json.dumps(EINSTEIN)
-back_to_dict = json.loads(einstein_json)
+einstein_json = json.dumps(EINSTEIN) #converts the above info (dictionary) into json
+back_to_dict = json.loads(einstein_json) #converst json to dictionary
 print(einstein_json)
 pprint(back_to_dict)
 
+#the following codes turns the .csv file into a .json file
 with open("laureates.csv", "r") as f:
     reader = csv.DictReader(f)
     laureates = list(reader)
